d590c570b84e5a317ff4c4a029c9769363e2f6f3
Daniel Weber <dantech88@yahoo.com>
2019-06-15 23:27:14 -0400

    init commit

---
+++ client.js
@@ -0,0 +1,43 @@
+let key_elt = document.getElementById("key");
+let messages_elt = document.getElementById("messages");
+let timeout_elt = document.getElementById("timeout");
+
+function get_secs() {
+    return new Date().getTime() / 1000;
+}
+
+let timeout_secs = 5 * 60;
+
+let serverConnection = new WebSocket('wss://' + window.location.hostname + ':3000');
+serverConnection.onmessage = (msg) => {
+    console.log("server message");
+    console.log(msg);
+    let data = JSON.parse(msg.data);
+    if ("id" in data) {
+        let url = "https://"+window.location.hostname +":3000/"+ data.id;
+        key_elt.innerHTML = "Send data to <a target=\"_blank\" href=\"" + url + "\">" + url +" </a>";
+        let connTime = get_secs();
+        timeout_elt.innerHTML = "Closing connection in " + timeout_secs + " seconds";
+        setInterval(() => {
+            let remaining = (timeout_secs - (get_secs() - connTime));
+            timeout_elt.innerHTML = "Closing connection in " + Math.round(remaining) + " seconds";
+            if (remaining <= 0) {
+                location.reload();
+            }
+        }, 1000);
+    } else if ("data" in data) {
+        let data_div = document.createElement("div");
+        jsonTree.create(data, data_div);
+        messages_elt.prepend(data_div);
+    }
+};
+serverConnection.onclose = (event) => {
+    console.log("closed websocket");
+}
+function send(data) {
+    serverConnection.send(JSON.stringify(data));
+}
+
+function close() {
+    serverConnection.close();
+}
---
+++ server.js
@@ -0,0 +1,82 @@
+const https = require('https');
+const fs = require('fs');
+const WebSocket = require('ws');
+const WebSocketServer = WebSocket.Server;
+
+const serverConfig = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+};
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+let honeypots = {};
+
+let timeout_secs = 5*60;
+
+// Create a server for the client html page
+const handleRequest = function (request, response) {
+    // Render the single client html file for any request the HTTP server receives
+    console.log('request received: ' + request.url);
+
+    if (request.url === '/') {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(fs.readFileSync('index.html'));
+    } else if (request.url === '/client.js') {
+        response.writeHead(200, { 'Content-Type': 'application/javascript' });
+        response.end(fs.readFileSync('client.js'));
+    } else if (request.url === '/jsonTree.js') {
+        response.writeHead(200, { 'Content-Type': 'application/javascript' });
+        response.end(fs.readFileSync('jsonTree.js'));
+    } else if (request.url === '/jsonTree.css') {
+        response.writeHead(200, { 'Content-Type': 'text/css' });
+        response.end(fs.readFileSync('jsonTree.css'));
+    } else if (request.url === '/icons.svg') {
+        response.writeHead(200, { 'Content-Type': 'image/svg+xml' });
+        response.end(fs.readFileSync('icons.svg'));
+    } else {
+        let base_idx = request.url.indexOf("/", 1);
+        let request_base = request.url.substring(1, base_idx == -1 ? undefined : base_idx);
+        console.log(request_base);
+        if (request_base in honeypots) {
+            let data = {};
+            data["url"] = request.url;
+            data["headers"] = request.headers;
+            data["connectio.remoteAddress"] = request.connection.remoteAddress;
+            data["time"] = new Date().toString();
+            honeypots[request_base].send(JSON.stringify({data: data}));
+            response.statusCode = 200;
+            response.end("OK");
+        } else {
+            response.writeHead(404);
+            response.end();
+        }
+    }
+};
+
+const httpsServer = https.createServer(serverConfig, handleRequest);
+httpsServer.listen(port, hostname);
+
+// Create a server for handling websocket calls
+const wss = new WebSocketServer({ server: httpsServer });
+
+function generate_id() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+
+    return s4() + s4();
+}
+
+wss.on('connection', function (ws) {
+    console.log('got ws connection');
+    let id = generate_id();
+    honeypots[id] = ws
+    ws.send(JSON.stringify({id: id}));
+    setTimeout(() => {
+        ws.close();
+        console.log("closed connection");
+        delete honeypots[id];
+    }, 1000*timeout_secs);
+});
